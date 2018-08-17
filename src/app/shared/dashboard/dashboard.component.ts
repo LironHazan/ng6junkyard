071fa94bf51dcb1d9f3cec41fcb96427bd6d87bd
lironh <lironenk@gmail.com>
2018-08-17 18:29:18 +0300

    :sparkle: configured to use sass

--- src/app/shared/dashboard/dashboard.component.ts
+++ src/app/shared/dashboard/dashboard.component.ts
@@ -5,7 +5,7 @@ import { Breakpoints, BreakpointState, BreakpointObserver } from '@angular/cdk/l
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
   /** Based on the screen size, switch from standard to one column per row */
